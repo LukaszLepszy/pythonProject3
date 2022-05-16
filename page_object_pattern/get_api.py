4c7e6e364d064a514431e463df2d05b183912904
lukasz <lukasz.lepszy@fnx-group.com>
2022-05-16 15:01:48 +0200

    Post_tests

--- page_object_pattern/get_api.py
+++ page_object_pattern/get_api.py
@@ -5,12 +5,9 @@ import pytest
 import requests
 
 
-class Getapi():
+class Getapi:
 
-    # def __init__(self, api_key):
-    #     self.api_key = api_key
-    #     self.base_url = "https://reqres.in/api"
-    #     self.new_data = {"name": "morpheus", "job": "leader"}
+    post_request_data = {"name": "morpheus", "job": "leader"}
 
     def api_test(self, api_key):
         BASE_URL = "https://reqres.in/api"
@@ -21,14 +18,6 @@ class Getapi():
         x = resp.json()
         data = x['data']
         return data
-
-    def get_keys_form_dict(self, api_key):
-        list_keys = []
-        for key, value in self.api_test(api_key).items():
-            list_keys.append(key)
-        return list_keys
-
-
 
     def get_properly_url(self, api_key):
         properly_url = "https://reqres.in/api" + f"/{api_key}"
@@ -51,6 +40,12 @@ class Getapi():
             list_keys.append(k)
         return list_keys
 
+    def get_value_from_dict(self, data):
+        list_values = []
+        for k, v in data.items():
+            list_values.append(v)
+        return list_values
+
     def get_keys_from_list_of_dicts(self, data):
         list_keys = []
         for x in data:
@@ -63,15 +58,14 @@ class Getapi():
         status = r.status_code
         return status
 
-    def post_data(self, properly_url):
-        self.new_data = {"name": "morpheus", "job": "leader"}
-        post = requests.post(properly_url, self.new_data)
-        return post.text
+    def post_request(self, properly_url, data):
+        post = requests.post(properly_url, data)
+        return post.json()
 
-    def post_register(self, properly_url):
-        self.register_data = { "email": "eve.holt@reqres.in", "password": "pistol"}
-        post = requests.post(properly_url, self.register_data)
-        return post.text
+    def post_status(self, properly_url, data):
+        post = requests.post(properly_url, data)
+        status = post.status_code
+        return status
 
     def loads(self):
         url = "https://reqres.in/api/users/2"
@@ -89,10 +83,10 @@ class Getapi():
         return pages
 
 
-API = Getapi()
-print(API.post_register(API.get_properly_url("register")))
-# print(type(API.loadss()))
-
-
-# print(API.get_keys(API.get_response_data(API.get_properly_url('users/2'))))
+# API = Getapi()
+# print(API.post_request(API.get_properly_url("users"), post_request_data))
+# # print(type(API.loadss()))
+#
+#
+# # print(API.get_keys(API.get_response_data(API.get_properly_url('users/2'))))
 
